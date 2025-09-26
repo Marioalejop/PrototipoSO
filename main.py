95d8cf92b3961b8967727c9231845c05734213df
ESTUDIANTE <marioalejop@gmail.com>
2025-09-25 21:26:57 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+from shell import Shell
+
+def main():
+    print("=== Prototipo de Sistema Operativo Básico ===")
+    shell = Shell()
+    shell.run()
+
+if __name__ == "__main__":
+    main()
